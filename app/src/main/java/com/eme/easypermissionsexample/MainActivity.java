bc06855d1ca4f0b536f18618e64afe3b88af7f91
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2020-07-08 13:05:39 -0400

    Proyecto de ejemplo con EasyPermissions

---
+++ app/src/main/java/com/eme/easypermissionsexample/MainActivity.java
@@ -0,0 +1,54 @@
+package com.eme.easypermissionsexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.BuildConfig;
+import pub.devrel.easypermissions.EasyPermissions;
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_CAMERA_AND_LOCATION = 7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initLog();
+    }
+
+    private void initLog() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @AfterPermissionGranted(RC_CAMERA_AND_LOCATION)
+    private void methodRequiresTwoPermission() {
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+
+            Timber.d("permisos otorgados");
+            // Already have permission, do the thing
+            // ...
+        } else {
+
+            Timber.d("permisos rechazados");
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this, getString(R.string.camera_and_location_rationale),
+                    RC_CAMERA_AND_LOCATION, perms);
+        }
+    }
+}
